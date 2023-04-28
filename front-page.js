dd9caecb4d7ab7d6d84d40e23bc0a9b0b7ac97d2
Kevin <kevin@jredu.net>
2023-04-28 13:17:11 +0800

    調整間隔

--- front-page.js
+++ front-page.js
@@ -1,26 +1,20 @@
-console.log("ok Go");
 $(document).ready(function(){
   $("#income-control").click(function(){
     $("#income-area").toggleClass("hide");
   });
   $("#registration-control").click(function(){
-    console.log("You Got It!!!");
     $("#registration-area").toggleClass("hide");
   });
   $("#repay-control").click(function(){
-    console.log("You Got It!!!");
     $("#repay-area").toggleClass("hide");
   });
   $("#birthday-control").click(function(){
-    console.log("You Got It!!!");
     $("#birthday-area").toggleClass("hide");
   });
   $("#student-day-off-control").click(function(){
-    console.log("You Got It!!!");
     $("#student-day-off-area").toggleClass("hide");
   });
   $("#employee-day-off-control").click(function(){
-    console.log("You Got It!!!");
     $("#employee-day-off-area").toggleClass("hide");
   });
 });
